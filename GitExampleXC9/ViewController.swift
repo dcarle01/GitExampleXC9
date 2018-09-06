ac721374b2fc1095335d0e3f40fa28fd03d778d5
Dale Carley <dale@opengateways.com>
2018-09-06 08:04:01 -0500

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Dale Carley on 9/6/18.
+//  Copyright © 2018 Dale Carley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
